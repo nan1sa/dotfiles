7e585a35d4f1835bba1e8f5f99fa1786b71e58d2
nan1sa <nan1sa@nan1.casa>
2021-02-15 10:18:31 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -eu
+
+cd
+git init
+git remote add origin git@github.com:nan1sa/dotfiles.git
+git fetch origin master
+git reset --hard origin/master
+echo '*' > .git/info/exclude
+
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd ../
+rm -rf yay-bin
+
+curl https://sh.rustup.rs -sSf | sh
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
